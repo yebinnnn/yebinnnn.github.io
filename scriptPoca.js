776db40e05ca0bfa78f659af4d3306e0dc00bbb5
Tahiel.Ludi <luditahiel@gmail.com>
2026-07-04 20:17:02 -0300

    actual dollar

--- scriptPoca.js
+++ scriptPoca.js
@@ -1,5 +1,5 @@
 function calcular() {
-    const dolar = 1520;
+    const dolar = 1530;
 
     var valorInput = document.getElementById("valor_usd").value;
     var valorNumerico = parseFloat(valorInput.replace(',', '.'));
